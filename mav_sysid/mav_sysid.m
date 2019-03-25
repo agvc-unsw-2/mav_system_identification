b541152bb8dd2ee7d051ca0bf278bab1929c7094
Hideyoshi Cheong <hideyoshi@live.com.au>
2019-03-25 11:21:20 +1100

    Updated mav_sysid script to remove NaNs and some timing issues

--- mav_sysid/mav_sysid.m
+++ mav_sysid/mav_sysid.m
@@ -1,15 +1,19 @@
 %% user set params
 
-bag_name = '2019-01-28-16-42-05.bag';
-imu_topic = '/simulation/uav1/ground_truth/mavros/imu/data';
-control_topic = '/simulation/uav1/command/roll_pitch_yawrate_thrust';
+bag_name = 'testBag.bag'; %'2019-03-23-14-04-15.bag';
+bag_name = '2019-03-23-14-04-15.bag';
+bag_name = '2019-03-23-16-01-20.bag';
+%bag_name = '2019-03-23-16-13-18.bag';
+bag_name = '2019-03-23-18-45-51.bag';
+imu_topic = '/mavros/imu/data';
+control_topic = '/mavros/setpoint_raw/roll_pitch_yawrate_thrust';
 
-sys_id_start_time_s = 10;
-sys_id_end_time_s = 130;
+% sys_id_start_time_s = 20;
+sys_id_end_time_s = 80;
 
 % parameters that must be provided 
-system_mass_kg = 1.65;
-linear_drag_coefficients = [0.02, 0.02, 0]; %default values work for most systems
+system_mass_kg = 1.0;
+linear_drag_coefficients = [0.01, 0.01, 0]; %default values work for most systems
 yaw_gain = 1.0; %default value works for most systems
 yaw_damping = 0.95; %default value works for most systems
 yaw_omega = 5.0; %default value works for most systems
@@ -31,9 +35,12 @@ attitude_cmd = readCommandRollPitchYawRateThrust(bag, control_topic);
 imu_data.rpy = quat2rpy([imu_data.q(4,:)', imu_data.q(1:3,:)']');
 attitude_cmd.rpy = vertcat(attitude_cmd.roll, attitude_cmd.pitch, attitude_cmd.yaw_rate);
 
-t_start = imu_data.t(1);
+
+t_start = imu_data.t(1);% + .6;
+% t_start = 0
 imu_data.t = imu_data.t - t_start;
-attitude_cmd.t = attitude_cmd.t - attitude_cmd.t(1);
+attitude_cmd.t = attitude_cmd.t - t_start 
+%attitude_cmd.t = attitude_cmd.t - attitude_cmd.t(1);
 %% plot
 figure(1);
 ax = axes;
@@ -72,7 +79,7 @@ grid on;
 ax.FontSize = 16;
 
 %% sysid
-
+%imu_data.t = imu_data.t + 0.6;
 attitude_cmd.rpy_interp = zeros(size(imu_data.rpy));
 attitude_cmd.rpy_interp(1,:) = interp1(attitude_cmd.t, attitude_cmd.rpy(1,:), imu_data.t);
 attitude_cmd.rpy_interp(2,:) = interp1(attitude_cmd.t, attitude_cmd.rpy(2,:), imu_data.t);
@@ -83,7 +90,7 @@ attitude_cmd.thrust_interp = interp1(attitude_cmd.t, attitude_cmd.thrust(3,:), i
 
 attitude_cmd.t = imu_data.t;
 
-imu_data.t = imu_data.t(imu_data.t > sys_id_start_time_s & imu_data.t < sys_id_end_time_s);
+imu_data.t = imu_data.t(imu_data.t > sys_id_start_time_s& imu_data.t < sys_id_end_time_s);
 imu_data.rpy = imu_data.rpy(:, imu_data.t > sys_id_start_time_s & imu_data.t < sys_id_end_time_s);
 imu_data.a = imu_data.a(:, imu_data.t > sys_id_start_time_s & imu_data.t < sys_id_end_time_s);
 
@@ -92,8 +99,14 @@ attitude_cmd.rpy_interp = attitude_cmd.rpy_interp(:, attitude_cmd.t > sys_id_sta
 attitude_cmd.thrust_interp = attitude_cmd.thrust_interp(:, attitude_cmd.t > sys_id_start_time_s & attitude_cmd.t < sys_id_end_time_s);
 
 dt = mean(diff(imu_data.t));
+idx = find(isnan(attitude_cmd.rpy_interp(1,:)));
 roll_data = iddata(imu_data.rpy(1,:)', attitude_cmd.rpy_interp(1,:)', dt);
 pitch_data = iddata(imu_data.rpy(2,:)', attitude_cmd.rpy_interp(2,:)', dt);
+
+roll_data = iddata(imu_data.rpy(1,idx(end)+1:end)', attitude_cmd.rpy_interp(1,idx(end)+1:end)', dt);
+pitch_data = iddata(imu_data.rpy(2,idx(end)+1:end)', attitude_cmd.rpy_interp(2,idx(end)+1:end)', dt);
+
+
 
 roll_tf = tfest( roll_data, 1, 0);
 pitch_tf = tfest( pitch_data, 1, 0);
